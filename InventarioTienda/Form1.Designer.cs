260e1f943fb05bc0e5840cd4e7cd5affa20a29e5
edisonmalan <edisonmalan187@outlook.com>
2022-03-13 21:06:13 -0500

    Actualizacion 2.0.1

--- InventarioTienda/Form1.Designer.cs
+++ InventarioTienda/Form1.Designer.cs
@@ -270,7 +270,7 @@ namespace InventarioTienda
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(36, 192);
+            this.button1.Location = new System.Drawing.Point(36, 193);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(87, 23);
             this.button1.TabIndex = 40;
